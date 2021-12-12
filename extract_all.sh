6fa5544c4ed384985b876d5e374c335018b7db54
Chirag Karia <trichiragkaria@gmail.com>
2021-12-12 16:14:30 -0500

    Update extract_all.sh script to add help doc

--- extract_all.sh
+++ extract_all.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-ARG1=${1:-archive}
-ARG2=${2:-extracted_data}
+for i in "$@"; do
+  case $i in
+    -s=*|--source=*)
+      source="${i#*=}"
+      shift # past argument=value
+      ;;
+    -d=*|--destination=*)
+      destination="${i#*=}"
+      shift # past argument=value
+      ;;
+    -h|--help)
+    echo "Usage:"
+    echo "./extract_all.sh --source=DIRECTORY_WITH_ARCHIVES --destination=DESTINATION_DIR_EXTRACTED_FILES"
+    exit 0
+  esac
+done
 
-echo "ls ${ARG1}/*.tgz | xargs -n1 tar -C ${ARG2} -zxvf"
+source=${source:-archive}
+destination=${destination:-extracted_data}
+
+ls ${source}/*.tgz | xargs -n1 tar -C ${destination} -zxvf
