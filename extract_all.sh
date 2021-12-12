f073d8d47c7f35e0b1405014c4741ca01b99b9b8
Chirag Karia <trichiragkaria@gmail.com>
2021-12-12 15:40:59 -0500

    Add extraction script

--- extract_all.sh
+++ extract_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-ARG1=${1:-extracted_data}
+ARG1=${1:-archive}
+ARG2=${2:-extracted_data}
 
-echo "ls *.tgz | xargs -n1 tar -C ${ARG1} -zxvf"
+echo "ls ${ARG1}/*.tgz | xargs -n1 tar -C ${ARG2} -zxvf"
